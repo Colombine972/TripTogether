4e33333a5df68ebea9b7236077fa4e01a2f160a2
Colombine972 <decidela972@me.com>
2026-06-07 23:07:12 +0200

    modified footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -8,16 +8,16 @@ function Footer() {
     <footer className="footer-container">
       <hr className="footer-divider" />
       <article className="footer-content">
-        <Link to="/" onClick={() => window.scrollTo({ top: 0 })}>
-          <div className="footer-left">
+        <div className="footer-left">
+          <Link to="/" onClick={() => window.scrollTo({ top: 0 })}>
             <img
               src="/logos/logo.png"
               alt="Trip Together logo"
               className="footer-logo"
             />
-            Trip Together
-          </div>
-        </Link>
+            <span>Trip Together</span>
+          </Link>
+        </div>
 
         <div className="footer-center">
           <Link to="/privacy">Politique de confidentialité</Link>
